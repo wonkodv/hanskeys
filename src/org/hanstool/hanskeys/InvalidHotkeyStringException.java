1b67032033197516437bedc7b8b710e9b58c93bf
Wonko der Verständige <wonko@hanstool.org>
2011-01-13 00:49:27 +0000

    javadoc gemacht

---
+++ src/org/hanstool/hanskeys/InvalidHotkeyStringException.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.hanstool.hanskeys;
+
+/**
+ *
+ * @author Wonko
+ */
+public class InvalidHotkeyStringException extends Exception {
+
+    /**
+     * Creates a new instance of <code>InvalidHotkeyStringException</code> without detail message.
+     */
+    public InvalidHotkeyStringException() {
+    }
+
+
+    /**
+     * Constructs an instance of <code>InvalidHotkeyStringException</code> with the specified detail message.
+     * @param msg the detail message.
+     */
+    public InvalidHotkeyStringException(String msg) {
+        super(msg);
+    }
+}
